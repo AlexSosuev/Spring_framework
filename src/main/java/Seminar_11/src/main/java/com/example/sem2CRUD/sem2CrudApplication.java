588f7396e8fb8706cb1bbc258cdf8a76abf9b108
Alexander Sosuev <sav_ml@mail.ru>
2024-05-22 14:57:13 +0500

    11 семинар + ДЗ (ver 1.0) - до установки Граффаны

---
+++ src/main/java/Seminar_11/src/main/java/com/example/sem2CRUD/sem2CrudApplication.java
@@ -0,0 +1,11 @@
+package com.example.sem2CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sem2CrudApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(sem2CrudApplication.class, args);
+	}
+}
